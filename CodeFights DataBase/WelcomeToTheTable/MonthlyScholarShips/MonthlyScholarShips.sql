4b1871f694c5b5d1e0b2faac09dbbfbcd7ad6382
LautaroNavarro <navarro_lautaro@hotmail.com>
2018-06-23 00:12:04 -0300

    Added MonthlyCholarShips

---
+++ CodeFights DataBase/WelcomeToTheTable/MonthlyScholarShips/MonthlyScholarShips.sql
@@ -0,0 +1,7 @@
+#By Lautaro Navarro
+
+/*Please add ; after each select statement*/
+CREATE PROCEDURE monthlyScholarships()
+BEGIN
+	SELECT id, scholarship / 12 as scholarship FROM scholarships;
+END
