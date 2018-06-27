7f883b610504ff7f4a5bfedb513552853398ecfa
LautaroNavarro <navarro_lautaro@hotmail.com>
2018-06-27 11:48:17 -0300

    suspectsInvestigation2

---
+++ CodeFights DataBase/Would you LIKE the Second Meal/suspectsInvestigation2/suspectsInvestigation2.sql
@@ -0,0 +1,7 @@
+#By Lautaro Navarro
+
+/*Please add ; after each select statement*/
+CREATE PROCEDURE suspectsInvestigation2()
+BEGIN
+	SELECT id, name, surname FROM Suspect WHERE height <= 170 OR LOWER(name) NOT LIKE  'b%' OR LOWER(surname) NOT LIKE  'gre_n' ORDER BY id ASC;
+END
