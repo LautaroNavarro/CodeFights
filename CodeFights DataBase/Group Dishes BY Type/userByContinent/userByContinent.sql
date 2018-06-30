71d5f8abf5a5df369e3d1cb2b8131c35f492ac2d
LautaroNavarro <navarro_lautaro@hotmail.com>
2018-06-29 22:17:27 -0300

    userByContinent

---
+++ CodeFights DataBase/Group Dishes BY Type/userByContinent/userByContinent.sql
@@ -0,0 +1,7 @@
+#By Lautaro Navarro
+
+/*Please add ; after each select statement*/
+CREATE PROCEDURE usersByContinent()
+BEGIN
+	SELECT continent, SUM(users) AS users FROM countries GROUP BY continent ORDER BY users DESC;
+END
